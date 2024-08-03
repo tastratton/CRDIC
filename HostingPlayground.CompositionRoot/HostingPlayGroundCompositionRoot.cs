de88f789a8ada511bc113d27fcc082b95b54d501
Thomas Stratton <tastratton@georgiasown.org>
2024-08-02 20:47:42 -0500

    move compositionroot to it's own project

---
+++ HostingPlayground.CompositionRoot/HostingPlayGroundCompositionRoot.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration; // TODO 1 - should we be using this directly here or passing args from program.cs?
+using OldGreeter;
+
+namespace HostingPlayground.CompositionRoot;
+public static class HostingPlayGroundCompositionRoot
+{
+    public static IServiceCollection AddServices(this IServiceCollection services)
+    {
+        services.AddSingleton<IGreeter, Greeter>();
+        return services;
+    }
+
+    public static Func<string[], IHostBuilder> GetHostBuilder = delegate (string[] args)
+    {
+        return Host.CreateDefaultBuilder(args)
+        .UseDefaultServiceProvider((context, options) =>
+        {
+            options.ValidateScopes = true;
+        })
+        .ConfigureHostConfiguration((hostConfig) =>
+        {
+            // see TODO 1
+            hostConfig.SetBasePath(Directory.GetCurrentDirectory());
+            hostConfig.AddJsonFile("appsettings.json", optional: true);
+            // end see TODO 1
+            //hostConfig.AddEnvironmentVariables(prefix: ""); // prefix: "PREFIX_"); // DOTNET_ by default already added
+            ;
+        })
+        .ConfigureAppConfiguration((hostContext, appConfig) =>
+        {
+            IHostEnvironment env = hostContext.HostingEnvironment;
+            Console.WriteLine($"isDevelopment: {env.IsDevelopment()}");
+            // see TODO 1
+            appConfig.AddCommandLine(args);
+            // end see TODO 1
+        })
+        ;
+
+    };
+
+    public static Action<IHostBuilder> ActionConfigureAppConfiguration = delegate (IHostBuilder builder)
+    {
+        builder.ConfigureServices((hostContext, services) =>
+        {
+            IConfiguration configuration = hostContext.Configuration;
+            services.AddSingleton<IGreeter, Greeter>();
+        });
+    };
+
+
+    public static Action<IHostBuilder> ActionConfigureServices = delegate (IHostBuilder builder)
+    {
+        builder.ConfigureServices((context, services) =>
+        {
+            // moved to compositionroot
+            //services.AddSingleton<IGreeter, Greeter>();
+            services.AddServices();
+        });
+    };
+
+
+
+}
--- HostingPlayground/Program.cs
+++ HostingPlayground/Program.cs
@@ -11,42 +11,18 @@ using static HostingPlayground.HostingPlaygroundLogEvents;
 using System;
 using Microsoft.Extensions.Configuration;
 using System.IO;
-using OldGreeter;
+using HostingPlayground.CompositionRoot;
 
 namespace HostingPlayground;
 
 class Program
 {
-    // for dependency injection container
-    private static Func<string[], IHostBuilder> GetHostBuilder = delegate (string[] args)
-    {
-        return Host.CreateDefaultBuilder(args)
-        .UseDefaultServiceProvider((context, options) =>
-        {
-            options.ValidateScopes = true;
-        })
-        .ConfigureHostConfiguration(hostConfig =>
-        {
-            hostConfig.SetBasePath(Directory.GetCurrentDirectory());
-            //hostConfig.AddJsonFile("hostsettings.json", optional: true);
-            //hostConfig.AddEnvironmentVariables(prefix: "PREFIX_");
-            //hostConfig.AddCommandLine(args);
-        });
-    };
-
-    private static Action<IHostBuilder> ActionConfigureServices = delegate (IHostBuilder builder)
-    {
-        builder.ConfigureServices((context, services) =>
-        {
-            services.AddSingleton<IGreeter, Greeter>();
-        });
-    };
-
     // command line
     static async Task<int> Main(string[] args)
     {
+        IHostBuilder hostBuilder = HostingPlayGroundCompositionRoot.GetHostBuilder(args);
         var commandLineParser = BuildCommandLine()
-            .UseHost(args => GetHostBuilder(args), ActionConfigureServices)
+            .UseHost(args => hostBuilder, HostingPlayGroundCompositionRoot.ActionConfigureServices)
             .UseDefaults()
             .Build();
         return await commandLineParser.InvokeAsync(args);
@@ -59,14 +35,14 @@ class Program
                 IsRequired = true
             }
         };
-        root.Handler = CommandHandler.Create<GreeterOptions, IHost>(Run);
+        root.Handler = CommandHandler.Create<OldGreeter.GreeterOptions, IHost>(Run);
         return new CommandLineBuilder(root);
     }
 
-    private static void Run(GreeterOptions options, IHost host)
+    private static void Run(OldGreeter.GreeterOptions options, IHost host)
     {
         var serviceProvider = host.Services;
-        var greeter = serviceProvider.GetRequiredService<IGreeter>();
+        var greeter = serviceProvider.GetRequiredService<OldGreeter.IGreeter>();
         var loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
         var logger = loggerFactory.CreateLogger(typeof(Program));
 
