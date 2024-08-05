f2f23f51b51999515a257cd20e40959466f010ba
Thomas Stratton <tastratton@georgiasown.org>
2024-08-05 16:36:14 -0500

    move commandline functions to Commands.cs

---
+++ HostingPlayground/Commands.cs
@@ -0,0 +1,58 @@
+﻿using HostingPlayground.CompositionRoot;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.CommandLine;
+using System.CommandLine.Builder;
+using System.CommandLine.Help;
+using System.CommandLine.Hosting;
+using System.CommandLine.NamingConventionBinder;
+using static HostingPlayground.HostingPlaygroundLogEvents;
+using System.Linq;
+
+namespace HostingPlayground;
+internal class Commands
+{
+    public static CommandLineBuilder BuildCommandLine()
+    {
+        var root = new RootCommand(@"return a greeting"){
+            new Option<string>(aliases: (["-n", "--name"]), description: "The name to add to the greeting" ){
+                IsRequired = true
+            }
+        };
+        root.Handler = CommandHandler.Create<OldGreeter.GreeterOptions, IHost>(Run);
+        var sub1Command = new Command("dotest", "an example subcommand named dotest");
+        root.AddCommand(sub1Command);
+
+        return new CommandLineBuilder(root);
+    }
+    public static System.CommandLine.Parsing.Parser GetCommandLineParser(IHostBuilder hostBuilder, string[] args)
+    {
+        System.CommandLine.Parsing.Parser parser = Commands.BuildCommandLine()
+            .UseHost(args => hostBuilder, HostingPlayGroundCompositionRoot.ActionConfigureServices)
+            .UseDefaults()
+            .UseHelp(ctx =>
+            {
+                ctx.HelpBuilder.CustomizeLayout(_ =>
+                    HelpBuilder.Default
+                    .GetLayout()
+                    .Append(_ => _.Output.WriteLine(""))
+                    .Append(_ => _.Output.WriteLine("Example:"))
+                    .Append(_ => _.Output.WriteLine("  $ HostingPlayground --name 'Joe'"))
+                    );
+            })
+            .Build();
+        return parser;
+    }
+    private static void Run(OldGreeter.GreeterOptions options, IHost host)
+    {
+        var serviceProvider = host.Services;
+        var greeter = serviceProvider.GetRequiredService<OldGreeter.IGreeter>();
+        var loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+        var logger = loggerFactory.CreateLogger(typeof(Program));
+
+        var name = options.Name;
+        logger.LogInformation(GreetEvent, "Greeting was requested for: {name}", name);
+        greeter.Greet(name);
+    }
+}
--- HostingPlayground/Program.cs
+++ HostingPlayground/Program.cs
@@ -1,17 +1,17 @@
 ﻿using System.CommandLine;
-using System.CommandLine.Builder;
-using System.CommandLine.Hosting;
-using System.CommandLine.NamingConventionBinder;
+//using System.CommandLine.Builder;
+//using System.CommandLine.Hosting;
+//using System.CommandLine.NamingConventionBinder;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+//using Microsoft.Extensions.Logging;
 using System.CommandLine.Parsing;
 using System.Threading.Tasks;
-using Microsoft.Extensions.DependencyInjection;
-using static HostingPlayground.HostingPlaygroundLogEvents;
+//using Microsoft.Extensions.DependencyInjection;
+//using static HostingPlayground.HostingPlaygroundLogEvents;
 using HostingPlayground.CompositionRoot;
 using Microsoft.Extensions.Configuration;
-using System.CommandLine.Help;
-using System.Linq;
+//using System.CommandLine.Help;
+//using System.Linq;
 
 
 namespace HostingPlayground;
@@ -23,23 +23,11 @@ class Program
         var configBuilder = new ConfigurationBuilder();
         ProgramConfig.GetConfig(args, configBuilder);
         IHostBuilder hostBuilder = HostingPlayGroundCompositionRoot.GetHostBuilder(args);
-        var commandLineParser = BuildCommandLine()
-            .UseHost(args => hostBuilder, HostingPlayGroundCompositionRoot.ActionConfigureServices)
-            .UseDefaults()
-            .UseHelp(ctx =>
-                {
-                    ctx.HelpBuilder.CustomizeLayout(_ =>
-                        HelpBuilder.Default
-                        .GetLayout()
-                        .Append(_ => _.Output.WriteLine(""))
-                        .Append(_ => _.Output.WriteLine("Example:"))
-                        .Append(_ => _.Output.WriteLine("  $ HostingPlayground --name 'Joe'"))
-                        );               
-                })
-            .Build();
+        Parser commandLineParser = Commands.GetCommandLineParser(hostBuilder, args);
         return await commandLineParser.InvokeAsync(args);
     }
 
+ /*
     private static CommandLineBuilder BuildCommandLine()
     {
         var root = new RootCommand(@"return a greeting"){
@@ -53,7 +41,9 @@ class Program
         
         return new CommandLineBuilder(root);
     }
+ */
 
+    /*
     private static void Run(OldGreeter.GreeterOptions options, IHost host)
     {
         var serviceProvider = host.Services;
@@ -65,4 +55,5 @@ class Program
         logger.LogInformation(GreetEvent, "Greeting was requested for: {name}", name);
         greeter.Greet(name);
     }
+    */
 }
