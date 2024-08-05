0412575b94b15b0bfd16d9100e71170ebe55ecaa
Thomas Stratton <tastratton@georgiasown.org>
2024-08-05 11:07:42 -0500

    move program config to it's own class

--- HostingPlayground/Program.cs
+++ HostingPlayground/Program.cs
@@ -8,12 +8,9 @@ using System.CommandLine.Parsing;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using static HostingPlayground.HostingPlaygroundLogEvents;
-using System;
+using HostingPlayground.CompositionRoot;
 using Microsoft.Extensions.Configuration;
 
-using System.IO;
-using HostingPlayground.CompositionRoot;
-using System.Collections.Immutable;
 
 namespace HostingPlayground;
 
@@ -21,39 +18,8 @@ class Program
 {
     static async Task<int> Main(string[] args)
     {
-        // add config processing to main so we can get the config before the DI container actually exists...
-        // that way, we can configure it from config
-
-        // get DOTNET_ENVIRONMENT... environment variables and commandline only first
         var configBuilder = new ConfigurationBuilder();
-        var configRoot = configBuilder
-            .AddEnvironmentVariables() //"DOTNET_"
-            .AddCommandLine(args)
-            .Build();
-        String EnvironmentName = configRoot["ENVIRONMENT"];
-        if (String.IsNullOrEmpty(EnvironmentName))
-        {
-            EnvironmentName = "Production";
-        };
-
-        foreach ((string key, string value) in configRoot.AsEnumerable().ToImmutableSortedDictionary())
-        {
-            Console.WriteLine($"'{key}' = '{value}'");
-        }
-
-        configRoot = configBuilder
-        .AddJsonFile(path: "appsettings.json", optional: false, reloadOnChange: false)
-        .AddJsonFile(path: $"appsettings.{EnvironmentName}.json", optional: true, reloadOnChange: false)
-        //.AddEnvironmentVariables()  // << if need ALL the env vars!
-        .AddEnvironmentVariables(prefix: "DOTNET_")  // just env vars starting with DOTNET_, strips the DOTNET_ from the result
-        .AddCommandLine(args)
-        .Build();
-
-        foreach ((string key, string value) in configRoot.AsEnumerable().ToImmutableSortedDictionary())
-        {
-            Console.WriteLine($"'{key}' = '{value}'");
-        }
-
+        ProgramConfig.GetConfig(args, configBuilder);
         IHostBuilder hostBuilder = HostingPlayGroundCompositionRoot.GetHostBuilder(args);
         var commandLineParser = BuildCommandLine()
             .UseHost(args => hostBuilder, HostingPlayGroundCompositionRoot.ActionConfigureServices)
---
+++ HostingPlayground/ProgramConfig.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Immutable;
+
+namespace HostingPlayground;
+
+/// <summary>
+/// Get Configuration for Program.cs needed before Dependency Injection Container.
+/// e.g. options that may be used to alter DI container setup later.
+/// </summary>
+internal class ProgramConfig
+{
+
+    // get DOTNET_ENVIRONMENT... environment variables and commandline only first
+    public static IConfigurationRoot GetConfig(string[] args, ConfigurationBuilder configBuilder)
+    {
+        var configRoot = configBuilder
+            .AddEnvironmentVariables() //"DOTNET_"
+            .AddCommandLine(args)
+            .Build();
+        String EnvironmentName = configRoot["ENVIRONMENT"];
+        if (String.IsNullOrEmpty(EnvironmentName))
+        {
+            EnvironmentName = "Production";
+        };
+
+        foreach ((string key, string value) in configRoot.AsEnumerable().ToImmutableSortedDictionary())
+        {
+            Console.WriteLine($"'{key}' = '{value}'");
+        }
+
+        configRoot = configBuilder
+        .AddJsonFile(path: "appsettings.json", optional: false, reloadOnChange: false)
+        .AddJsonFile(path: $"appsettings.{EnvironmentName}.json", optional: true, reloadOnChange: false)
+        //.AddEnvironmentVariables()  // << if need ALL the env vars!
+        .AddEnvironmentVariables(prefix: "DOTNET_")  // just env vars starting with DOTNET_, strips the DOTNET_ from the result
+        .AddCommandLine(args)
+        .Build();
+
+        foreach ((string key, string value) in configRoot.AsEnumerable().ToImmutableSortedDictionary())
+        {
+            Console.WriteLine($"'{key}' = '{value}'");
+        }
+        return configRoot;
+    }
+
+}
